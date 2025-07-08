3c79a8f06b4b35a6cdf013a29f1e4f4f65697f9d
freelanceradnan <reactorbro722@gmail.com>
2025-07-08 23:56:46 +0600

    javascript typeconversion number to boolean added

--- typeconversion.js
+++ typeconversion.js
@@ -1,16 +1,21 @@
-// //javascript type conversion(number to String):
+// // //javascript type conversion(number to String):
+// let num=10;
+// let StrFromNum=String(num);
+// console.log(StrFromNum);
+
+// //javascript type conversion(String to number):
+// let str='10';
+// let NumFromStr=Number(str);
+// console.log(typeof NumFromStr);
+
+// //javascript type conversion(Boolean  to number):
+// let isBool=true;
+// let NumFromNum=Boolean(isBool);
+// console.log(NumFromNum);
+
+//javascript type conversion(Number to Boolean):
 let num=10;
-let StrFromNum=String(num);
-console.log(StrFromNum);
-
-//javascript type conversion(String to number):
-let str='10';
-let NumFromStr=Number(str);
-console.log(typeof NumFromStr);
-
-//javascript type conversion(Boolean  to number):
-let isBool=true;
-let NumFromNum=Boolean(isBool);
-console.log(NumFromNum);
+let BoolFromNum=Boolean(num);
+console.log(BoolFromNum);
 
 
