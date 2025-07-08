24bcec041c9e2d54d7fba16dae60c3ab8b765c94
freelanceradnan <reactorbro722@gmail.com>
2025-07-09 02:03:06 +0600

    decrement operator added

--- operator.js
+++ operator.js
@@ -1,34 +1,39 @@
-//javascript operators(addition):
-let x=10;
-let y=20;
-console.log(x+y);
+// //javascript operators(addition):
+// let x=10;
+// let y=20;
+// console.log(x+y);
 
-//javascript operators(addition with string):
-let x='10';
-let y=20;
-console.log(x+y);
+// //javascript operators(addition with string):
+// let x='10';
+// let y=20;
+// console.log(x+y);
 
-//javascript operator(substruction):
-let x=20;
-let y=10;
-console.log(x-y);
+// //javascript operator(substruction):
+// let x=20;
+// let y=10;
+// console.log(x-y);
 
-//javascript operator (multiplication):
-let x=20;
-let y=20;
-console.log(x*y);
+// //javascript operator (multiplication):
+// let x=20;
+// let y=20;
+// console.log(x*y);
 
-//javascript operator(exponentiation)
-let x=20;
-let y=20;
-console.log(x**y);
+// //javascript operator(exponentiation)
+// let x=20;
+// let y=20;
+// console.log(x**y);
 
-//javascript operators(increment):
-let num=10;
-num++;
-console.log(num);
+// //javascript operators(increment):
+// let num=10;
+// num++;
+// console.log(num);
 
-//javascript operator(increment):
-let num=20;
-num2=num++;
-console.log(num2);
+// //javascript operator(increment):
+// let num=20;
+// num2=num++;
+// console.log(num2);
+
+//javascript operator (decrement):
+let age=20;
+age--;
+console.log(age);
