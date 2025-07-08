81b14eecea62da2a10f4498da55662b2ccd4f55c
freelanceradnan <reactorbro722@gmail.com>
2025-07-08 23:27:54 +0600

    javascript const variable added

--- variable.js
+++ variable.js
@@ -1,8 +1,14 @@
 //javascript variable (let)
-// let x=10;
-// let y=20;
-// console.log(x+y);
+let x=10;
+let y=20;
+console.log(x+y);
+
 //javascript variable (var)
 var x=10;
 var y=20;
+console.log(x+y);
+
+//javascript varable (const)
+const x=10;
+const y=10;
 console.log(x+y);
