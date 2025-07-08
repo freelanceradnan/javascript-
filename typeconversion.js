44291a4d8cdf106aec08aa88270fb0e57b99aae8
freelanceradnan <reactorbro722@gmail.com>
2025-07-08 23:49:35 +0600

    js typeconversion string to number added

--- typeconversion.js
+++ typeconversion.js
@@ -1,4 +1,9 @@
+// //javascript type conversion(number to String):
+// let num=10;
+// let StrFromNum=String(num);
+// console.log(StrFromNum);
+
 //javascript type conversion(String to number):
-let num=10;
-let StrFromNum=String(num);
-console.log(typeof StrFromNum);
+let str='10';
+let NumFromStr=Number(str);
+console.log(typeof NumFromStr);
