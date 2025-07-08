1ff221fb7a98eca46e7c879cb7476178dab2428a
freelanceradnan <reactorbro722@gmail.com>
2025-07-09 01:44:44 +0600

    operator substraction added

--- operator.js
+++ operator.js
@@ -1,9 +1,14 @@
-//javascript operators(addition):
-let x=10;
-let y=20;
-console.log(x+y);
+// //javascript operators(addition):
+// let x=10;
+// let y=20;
+// console.log(x+y);
 
-//javascript operators(addition):
-let x='10';
-let y=20;
-console.log(x+y);
+// //javascript operators(addition with string):
+// let x='10';
+// let y=20;
+// console.log(x+y);
+
+//javascript operator(substruction):
+let x=20;
+let y=10;
+console.log(x-y);
