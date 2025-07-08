651785aa32c82b6a5f46444c8555227c40e5ccd9
freelanceradnan <reactorbro722@gmail.com>
2025-07-09 02:34:15 +0600

    operator lessthen added

--- operator.js
+++ operator.js
@@ -1,80 +1,85 @@
-//javascript operators(addition):
-let x=10;
-let y=20;
-console.log(x+y);
+// //javascript operators(addition):
+// let x=10;
+// let y=20;
+// console.log(x+y);
 
-//javascript operators(addition with string):
-let x='10';
-let y=20;
-console.log(x+y);
+// //javascript operators(addition with string):
+// let x='10';
+// let y=20;
+// console.log(x+y);
 
-//javascript operator(substruction):
+// //javascript operator(substruction):
+// let x=20;
+// let y=10;
+// console.log(x-y);
+
+// //javascript operator (multiplication):
+// let x=20;
+// let y=20;
+// console.log(x*y);
+
+// //javascript operator(exponentiation)
+// let x=20;
+// let y=20;
+// console.log(x**y);
+
+// //javascript operators(increment):
+// let num=10;
+// num++;
+// console.log(num);
+
+// //javascript operator(increment):
+// let num=20;
+// num2=num++;
+// console.log(num2);
+
+// //javascript operator (decrement):
+// let age=20;
+// age--;
+// console.log(age);
+
+// //javascript operator (decrement)
+// let num=20;
+// num2=--num;
+// console.log(num2);
+
+// //operator(division):
+// let x=100;
+// let y=10;
+// console.log(x/y);
+
+// //operator(modulus):
+// let x=110;
+// let y=12;
+// console.log(x%y);
+
+// //javascript comparison operators:
+// //javascript operator(equals to):
+// let x=20;
+// let y=10;
+// console.log(x==y);
+
+// //javascript operator(equals value or equal type):
+// let x=20;
+// let y=10;
+// console.log(x===y);
+
+// //javascript operator(not equal):
+// let x=20;
+// let y=20;
+// console.log(!x==y);
+
+// //operator (not equal type or value):
+// let x=20;
+// let y=20;
+// console.log(x!==y);
+
+// //operator (greater then):
+// let x=20;
+// let y=10;
+// console.log(x>y);
+
+//operator(lessthen):
 let x=20;
 let y=10;
-console.log(x-y);
-
-//javascript operator (multiplication):
-let x=20;
-let y=20;
-console.log(x*y);
-
-//javascript operator(exponentiation)
-let x=20;
-let y=20;
-console.log(x**y);
-
-//javascript operators(increment):
-let num=10;
-num++;
-console.log(num);
-
-//javascript operator(increment):
-let num=20;
-num2=num++;
-console.log(num2);
-
-//javascript operator (decrement):
-let age=20;
-age--;
-console.log(age);
-
-//javascript operator (decrement)
-let num=20;
-num2=--num;
-console.log(num2);
-
-//operator(division):
-let x=100;
-let y=10;
-console.log(x/y);
-
-//operator(modulus):
-let x=110;
-let y=12;
-console.log(x%y);
-
-//javascript comparison operators:
-//javascript operator(equals to):
-let x=20;
-let y=10;
-console.log(x==y);
-
-//javascript operator(equals value or equal type):
-let x=20;
-let y=10;
-console.log(x===y);
-
-//javascript operator(not equal):
-let x=20;
-let y=20;
-console.log(!x==y);
-
-//operator (not equal type or value):
-let x=20;
-let y=20;
-console.log(x!==y);
-
-//operator (greater then):
-let x=20;
-let y=10;
-console.log(x>y);
+console.log(x<y);
