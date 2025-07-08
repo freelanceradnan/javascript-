85ddfa9b0d3d4658dc2c5dca36e7c0270555db72
freelanceradnan <reactorbro722@gmail.com>
2025-07-09 02:01:30 +0600

    increment operator added

--- operator.js
+++ operator.js
@@ -1,32 +1,32 @@
-// //javascript operators(addition):
-// let x=10;
-// let y=20;
-// console.log(x+y);
+//javascript operators(addition):
+let x=10;
+let y=20;
+console.log(x+y);
 
-// //javascript operators(addition with string):
-// let x='10';
-// let y=20;
-// console.log(x+y);
+//javascript operators(addition with string):
+let x='10';
+let y=20;
+console.log(x+y);
 
-// //javascript operator(substruction):
-// let x=20;
-// let y=10;
-// console.log(x-y);
+//javascript operator(substruction):
+let x=20;
+let y=10;
+console.log(x-y);
 
-// //javascript operator (multiplication):
-// let x=20;
-// let y=20;
-// console.log(x*y);
+//javascript operator (multiplication):
+let x=20;
+let y=20;
+console.log(x*y);
 
-// //javascript operator(exponentiation)
-// let x=20;
-// let y=20;
-// console.log(x**y);
+//javascript operator(exponentiation)
+let x=20;
+let y=20;
+console.log(x**y);
 
-// //javascript operators(increment):
-// let num=10;
-// num++;
-// console.log(num);
+//javascript operators(increment):
+let num=10;
+num++;
+console.log(num);
 
 //javascript operator(increment):
 let num=20;
