bb0f200b0c91fd5038097796c2c577504b53fd4b
freelanceradnan <reactorbro722@gmail.com>
2025-07-09 21:55:16 +0600

    for loop for print 1 to 100 and 100 tp 1

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 // //javascript loops
-// //using for loops for(print 1 to 100):
-// for(let i=0;i<=10;i++){
-//     console.log(i);
-// }
+//using for loops for(print 1 to 100):
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
 
 //using for loops(print 100 to 1):
 for(i=100;i>=0;i--){
