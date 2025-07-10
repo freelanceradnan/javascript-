a104bbe1534d7a5b813efcd6ee3a583590d7c974
freelanceradnan <reactorbro722@gmail.com>
2025-07-10 19:07:43 +0600

    git added basic array

---
+++ array.js
@@ -0,0 +1,7 @@
+//javascript array
+
+//javascript array and accessing
+let array=['adnan','shaharia','shaharia',25];
+console.log(array[3]);
+let array=['adnan','shaharia','mahim',25];
+console.log(array);
