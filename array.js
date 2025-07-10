a87321267ff447c8afb3b457b460aecb2e953b89
freelanceradnan <reactorbro722@gmail.com>
2025-07-10 19:14:46 +0600

    adding last value in array

--- array.js
+++ array.js
@@ -5,3 +5,9 @@ let array=['adnan','shaharia','shaharia',25];
 console.log(array[3]);
 let array=['adnan','shaharia','mahim',25];
 console.log(array);
+
+//adding value in array:(in last)
+
+let array=['adnan','shaharia','mahim',20];
+array[array.length]='no';
+console.log(array);
