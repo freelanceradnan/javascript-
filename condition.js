9695fe7e2c114685056fd2d62cb8ae3e465c6ccc
freelanceradnan <reactorbro722@gmail.com>
2025-07-11 23:36:17 +0600

    if else condition added

--- condition.js
+++ condition.js
@@ -1,7 +1,17 @@
-//condition in js
-//if else//
-let age=18;
+// //condition in js
+// //if condition//
+// let age=18;
+// let addr='Dhaka';
+// if(age>=18&&addr=='Dhaka'){
+//     console.log('you are right place!');
+// }
+
+//if else condition in js
+let age=10;
 let addr='Dhaka';
 if(age>=18&&addr=='Dhaka'){
-    console.log('you are right place!');
+    console.log('you are welcome');
+}
+else if(age){
+    console.log('you are get out');
 }
