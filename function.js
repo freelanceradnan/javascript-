c9c9d2dd326e104348aa844b1bb62948611eb86b
freelanceradnan <reactorbro722@gmail.com>
2025-07-13 02:32:49 +0600

    function with no paramenter value

--- function.js
+++ function.js
@@ -29,3 +29,17 @@ let result2=add(50,30);
 let result3=add(100,20);
 let result4=add(150,200);
 console.log(result1,result2,result3,result4);
+
+//function with no parameter value;
+function add(num1,num2){
+    if(!num1){
+        num1=0;
+    }
+    if(!num2){
+        num2=0;
+    }
+    return num1+num2;
+}
+let result1=add();
+
+console.log(result1);
