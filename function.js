b29b7e76254d8ad70a2e0c134b38ca389c69c146
freelanceradnan <reactorbro722@gmail.com>
2025-07-13 02:14:52 +0600

    function with different parameters

--- function.js
+++ function.js
@@ -11,3 +11,11 @@ function add(num1,num2){
     console.log(num1+num2);
 }
 add(10,10);
+
+//function with different paramenter;
+function sayHello(username){
+    console.log('Hello',username);
+}
+sayHello('Mahim');
+sayHello('Ridoy');
+sayHello('Nahid');
