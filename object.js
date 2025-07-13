52fddeea0b4bbff7ecab7bfe443b0d3c3d8a38ca
freelanceradnan <reactorbro722@gmail.com>
2025-07-13 23:34:55 +0600

    object literal and basic access

---
+++ object.js
@@ -0,0 +1,10 @@
+//javascript object and this keywords
+//object literal and accessing
+let person={
+    name:'adnan',
+    age:20,
+    sayHello(){
+        console.log('Hello world');
+    },
+}
+console.log(person.name);
