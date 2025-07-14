12198756be5a83aa7e5cf2fb98cf316e2f3e874a
freelanceradnan <reactorbro722@gmail.com>
2025-07-15 01:49:34 +0600

    rest parameter added

--- parameter.js
+++ parameter.js
@@ -9,3 +9,12 @@ function add(...nums){
     console.log(nums);
 }
 add(2,3,4,5,6);
+
+function add (...nums){
+    let result=0;
+    nums.forEach((el)=>{
+        result +=el;
+    });
+    return result;
+}
+console.log(add(1,2,3,4,5));
