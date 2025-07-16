aae54439af536ed38078b8b47c2066c8743d1ad3
freelanceradnan <reactorbro722@gmail.com>
2025-07-16 22:38:32 +0600

    javascript dom access with id

--- typeconversion.js
+++ typeconversion.js
@@ -1,29 +1,29 @@
-// // //javascript type conversion(number to String):
-// let num=10;
-// let StrFromNum=String(num);
-// console.log(StrFromNum);
+// //javascript type conversion(number to String):
+let num=10;
+let StrFromNum=String(num);
+console.log(StrFromNum);
 
-// //javascript type conversion(String to number):
-// let str='10';
-// let NumFromStr=Number(str);
-// console.log(typeof NumFromStr);
+//javascript type conversion(String to number):
+let str='10';
+let NumFromStr=Number(str);
+console.log(typeof NumFromStr);
 
-// //javascript type conversion(Boolean  to number):
-// let isBool=true;
-// let NumFromNum=Boolean(isBool);
-// console.log(NumFromNum);
+//javascript type conversion(Boolean  to number):
+let isBool=true;
+let NumFromNum=Boolean(isBool);
+console.log(NumFromNum);
 
-// //javascript type conversion(Number to Boolean):
-// let num=10;
-// let BoolFromNum=Boolean(num);
-// console.log(BoolFromNum);
+//javascript type conversion(Number to Boolean):
+let num=10;
+let BoolFromNum=Boolean(num);
+console.log(BoolFromNum);
 
-// //javascript type conversion(String to Boolean):
-// let str='10';
-// let BoolFromStr=Boolean(str);
-// console.log(BoolFromStr);
+//javascript type conversion(String to Boolean):
+let str='10';
+let BoolFromStr=Boolean(str);
+console.log(BoolFromStr);
 
 //javascript type conversion(Boolean to String):
 let myName=true;
 let StrFromBool=String(myName);
-console.log(typeof StrFromBool);
+console.log(StrFromBool);
